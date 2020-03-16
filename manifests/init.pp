5159a30903431730fae6d786bbb8001b33fb68ff
Yorick Poels <yorick.poels@gmail.com>
2020-03-16 16:10:07 +0100

    Add puppet strings info - change $ensure param type

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,14 @@
 # Class: quota
 #
+# @param packages
+#   Specifies which packages to install for managing quota. Differs per operating system family
+#
+# @param ensure
+#   Wether to install the quota package, and what version to install. 
 #
 class quota(
   Array $packages,
-  Enum['present', 'absent', 'installed', 'latest'] $ensure,
+  String $ensure,
 
 ){
 
